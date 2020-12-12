6df898f2ff4e58c979449f37c2c9deedf6ed7878
Edgar Ochoa <eochoa3@ucol.mx>
2020-12-12 16:10:18 -0600

    improve form styles

--- src/scenes/Home/components/Contact/index.js
+++ src/scenes/Home/components/Contact/index.js
@@ -5,7 +5,6 @@ function Contact() {
   return (
     <section className="site-section">
       <h2 id="contact">Contact</h2>
-      <p>Let's get in touch!</p>
       <ContactForm />
     </section>
   );
@@ -13,11 +12,15 @@ function Contact() {
 
 export const ContactForm = () => {
 
-  const [contact, setContact] = useState({
+  const initialState = {
     name: "",
     email: "",
     message: ""
-  })
+  }
+  const [contact, setContact] = useState(initialState)
+  const [success, setSuccess] = useState(false)
+  const [error, setError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
   function encode(data) {
     return Object.keys(data).map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])).join("&")
@@ -25,6 +28,7 @@ export const ContactForm = () => {
   function handleSubmit(event) {
     event.preventDefault();
     const { email, message, name } = event.target;
+    setIsLoading(true)
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -34,6 +38,14 @@ export const ContactForm = () => {
         message: message.value,
         name: name.value
       })
+    }).then((res) => {
+      if (res.ok) {
+        setSuccess(true)
+        setContact(initialState)
+      } else {
+        setError(true)
+      }
+      setIsLoading(false)
     })
   }
 
@@ -42,15 +54,20 @@ export const ContactForm = () => {
   }
 
   return (
-    <form className="contact-form" data-netlify="true" name="contactForm" onSubmit={handleSubmit} >
-      <label>Name</label>
-      <input name="name" type="text" value={contact.name} onChange={handleChange} />
-      <label>Email</label>
-      <input name="email" type="email" value={contact.email} onChange={handleChange} />
-      <label>Message</label>
-      <textarea name="message" rows={3} value={contact.message} onChange={handleChange} />
-      <button type="submit">Send</button>
-    </form>
+    <>
+      <p>Let's get in touch!</p>
+      { error && <p className="error">Oops! There was an error, try again</p>}
+      { success && <p className="success">Your message was sent, thank you!</p>}
+      <form className="contact-form" data-netlify="true" name="contactForm" onSubmit={handleSubmit} >
+        <label>Name</label>
+        <input name="name" type="text" value={contact.name} onChange={handleChange} required />
+        <label>Email</label>
+        <input name="email" type="email" value={contact.email} onChange={handleChange} required />
+        <label>Message</label>
+        <textarea name="message" rows={3} value={contact.message} onChange={handleChange} required />
+        <button type="submit" className={isLoading ? "loading" : null} disabled={isLoading}>Send</button>
+      </form>
+    </>
   );
 }
 
