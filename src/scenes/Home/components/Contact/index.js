e77baf03f8a425faf5914953363a7f057bc87fd4
Edgar Ochoa <eochoa3@ucol.mx>
2020-12-12 14:49:58 -0600

    check if form works now

--- src/scenes/Home/components/Contact/index.js
+++ src/scenes/Home/components/Contact/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 function Contact() {
@@ -6,17 +6,51 @@ function Contact() {
     <section className="site-section">
       <h2 id="contact">Contact</h2>
       <p>Let's get in touch!</p>
-      <form className="contact-form" name="contact">
-        <input type="hidden" name="form-name" value="contact" />
-        <label>Name</label>
-        <input name="name" type="text" />
-        <label>Email</label>
-        <input name="email" type="email" />
-        <label>Message</label>
-        <textarea name="message" rows={3} />
-        <button type="submit">Send</button>
-      </form>
+      <ContactForm />
     </section>
+  );
+}
+
+export const ContactForm = () => {
+
+  const [contact, setContact] = useState({
+    name: "",
+    email: "",
+    message: ""
+  })
+
+  function encode(data) {
+    return Object.keys(data).map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])).join("&")
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    const { email, message, name } = event.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": event.target.getAttribute("name"),
+        email: email.value,
+        message: message.value,
+        name: name.value
+      })
+    })
+  }
+
+  function handleChange(event) {
+    setContact({ ...contact, [event.target.name]: event.target.value })
+  }
+
+  return (
+    <form className="contact-form" data-netlify="true" name="contactForm" onSubmit={handleSubmit} >
+      <label>Name</label>
+      <input name="name" type="text" value={contact.name} onChange={handleChange} />
+      <label>Email</label>
+      <input name="email" type="email" value={contact.email} onChange={handleChange} />
+      <label>Message</label>
+      <textarea name="message" rows={3} value={contact.message} onChange={handleChange} />
+      <button type="submit">Send</button>
+    </form>
   );
 }
 
